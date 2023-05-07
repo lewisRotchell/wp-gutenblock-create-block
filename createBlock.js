5aebfcb74bac6f005b574e7b8d1e47ab208093c6
Lewis Rotchell <rotchell92@gmail.com>
2023-05-07 10:00:07 +0100

    init

---
+++ createBlock.js
@@ -0,0 +1,153 @@
+const fs = require("fs").promises;
+
+const prefix = process.argv[2] || "block";
+
+const blockName = process.argv[3] || "block";
+
+const blockNamePhp = blockName.replace(/-/g, "_");
+
+//block.json title
+const blockTitle = blockName
+  .replace(/-/g, " ")
+  .replace(/\w\S*/g, (w) => w.replace(/^\w/, (c) => c.toUpperCase()));
+
+let renderType = process.argv[4];
+
+const directoryName = `./src/blocks/${blockName}`;
+
+let saveJs = "";
+let renderPhp = "";
+
+const blockJson = {
+  $schema:
+    "https://raw.githubusercontent.com/WordPress/gutenberg/trunk/schemas/json/block.json",
+  apiVersion: 2,
+  version: "1.0.0",
+  name: `${prefix}/${blockName}`,
+  title: blockTitle,
+  icon: "smiley",
+  textdomain: prefix,
+  attributes: {},
+  supports: {
+    html: false,
+  },
+  editorScript: "file:./index.js",
+  editorStyle: "file:./index.css",
+  style: [`${blockName}-style-frontend`],
+};
+
+const indexJs = `
+import { registerBlockType } from '@wordpress/blocks';
+
+import "./style.css";
+import "./editor.css";
+
+import edit from './edit';
+import save from './save';
+
+registerBlockType('${prefix}/${blockName}', {
+  edit,
+  save,
+});`;
+
+const editJs = `
+import { useBlockProps } from '@wordpress/block-editor';
+
+const Edit = ({ attributes, setAttributes }) => {
+  return (
+    <div className="{...useBlockProps}">
+        <h1>${blockName}</h1>
+    </div>
+  );
+};
+
+export default Edit;`;
+
+if (renderType === "js" || renderType === "both") {
+  saveJs = `
+    import { useBlockProps } from '@wordpress/block-editor';
+
+    const Save = ({attributes}) => {
+        const {text, alignment} = attributes;
+        return (
+            <div className="{...useBlockProps.save()}">
+                <h1>${blockName}</h1>
+            </div>
+        );
+    }
+
+    export default Save;`;
+}
+
+if (renderType === "php") {
+  saveJs = `
+  export default function Save() {
+    return null;
+  }
+  `;
+}
+
+if (renderType === "php" || renderType === "both") {
+  renderPhp = `
+    <?php
+    function ${blockNamePhp}_render_cb( $attributes) {
+        ob_start(); ?>
+        <div class="${prefix}-${blockName}">
+            <h1>${blockName}</h1>
+        </div>
+        <?php
+        return ob_get_clean();
+    }
+    `;
+}
+
+const editorCss = ``;
+const styleCss = ``;
+
+async function createBlock() {
+  if (renderType != "php" && renderType != "js" && renderType != "both") {
+    console.error("Please specify a render type: php or js or both");
+    return;
+  }
+  try {
+    await fs.mkdir(directoryName);
+    console.log("Directory created successfully!");
+
+    await fs.writeFile(
+      `${directoryName}/block.json`,
+      JSON.stringify(blockJson, null, 2)
+    );
+    console.log(`File "${directoryName}/block.json" created.`);
+
+    await fs.writeFile(`${directoryName}/index.js`, indexJs);
+    console.log(`File "${directoryName}/index.js" created.`);
+
+    await fs.writeFile(`${directoryName}/edit.js`, editJs);
+    console.log(`File "${directoryName}/edit.js" created.`);
+
+    await fs.writeFile(`${directoryName}/save.js`, saveJs);
+    console.log(`File "${directoryName}/save.js" created.`);
+
+    if (renderPhp) {
+      await fs.writeFile(`${directoryName}/render.php`, renderPhp);
+      console.log(`File "${directoryName}/render.php" created.`);
+    }
+
+    await fs.writeFile(`${directoryName}/editor.css`, editorCss);
+    console.log(`File "${directoryName}/editor.css" created.`);
+
+    await fs.writeFile(`${directoryName}/style.css`, styleCss);
+    console.log(`File "${directoryName}/style.css" created.`);
+  } catch (err) {
+    if (err.code === "EEXIST") {
+      console.error("Block already exists! Please choose another name");
+      return;
+    }
+    console.error(err);
+    return;
+  }
+
+  console.log(renderType);
+}
+
+createBlock();
